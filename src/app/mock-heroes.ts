4bf98bc2f83c5e5e00d08fbfa9d4915c5f617f9f
MarySweetRollStolen <marylostherself.com>
2023-01-03 15:58:25 +0200

    Add tutor functionality

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Cat' },
+  { id: 13, name: 'Bombakitty' },
+  { id: 14, name: 'Cat' },
+  { id: 15, name: 'Magnetcat' },
+  { id: 16, name: 'RubberCaty' },
+  { id: 17, name: 'Kitty' },
+  { id: 18, name: 'Dr. Kitty' },
+  { id: 19, name: 'MagmaKitty' },
+  { id: 20, name: 'TornadoCat' }
+];
