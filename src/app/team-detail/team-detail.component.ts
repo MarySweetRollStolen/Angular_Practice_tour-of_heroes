1863c5a4a46e18e600e6dcbf4c386f0ec1c63716
MarySweetRollStolen <marylostherself.com>
2023-01-09 10:57:36 +0200

    Add adding new heros in team

--- src/app/team-detail/team-detail.component.ts
+++ src/app/team-detail/team-detail.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Team } from '../team';
 import { TeamService } from '../team.service';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 
 @Component({
@@ -12,19 +16,52 @@ import { TeamService } from '../team.service';
 })
 export class TeamDetailComponent {
   team: Team | undefined;
+  heroes$!: Observable<Hero[]>;
+  allHeros: Hero[] = [];
+  private searchTerms = new Subject<string>();
 
   constructor(
     private route: ActivatedRoute,
     private teamService: TeamService,
-    private location: Location
+    private location: Location,
+    private heroService: HeroService
   ) { }
 
   ngOnInit(): void {
     this.getTeam();
+    this.getHeroes();
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
+
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.allHeros = heroes);
   }
 
   getTeam(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
-    this.teamService.getTeam(id);
+    this.team = this.teamService.getTeam(id);
   }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  addNewMember(hero: Hero): void {
+    if(this.team){
+      this.team.members.push(hero);
+    };  
+  }
+
+
+
 }
